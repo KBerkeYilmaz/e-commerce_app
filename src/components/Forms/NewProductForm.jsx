c03413f6fc485c7c89d44955e8ae3bfa33783e90
Kutalmış Berke Yılmaz <kberkeyilmaz@gmail.com>
2023-07-22 15:48:13 +0300

    Previous changes didn't applied, pushing again.

--- src/components/Forms/NewProductForm.jsx
+++ src/components/Forms/NewProductForm.jsx
@@ -7,9 +7,6 @@ import BookProperties from "./FormItems/__TypeProperties/BookProperties";
 import { useFetch } from "../Hooks/useFetch";
 import { mutate } from "swr";
 import { API_URL } from "../../config";
-import { useStore } from "../../state/store";
-import axios from "axios";
-
 
 const NewProductForm = () => {
   const { register, handleSubmit, formState, reset, watch } = useForm();
@@ -32,14 +29,22 @@ const NewProductForm = () => {
       product_type: data.productType,
     };
     switch (data.productType) {
-      case 'dvd':
+      case "dvd":
         formData = { ...formData, size: data.size ? Number(data.size) : null };
         break;
-      case 'book':
-        formData = { ...formData, weight: data.weight ? Number(data.weight) : null };
+      case "book":
+        formData = {
+          ...formData,
+          weight: data.weight ? Number(data.weight) : null,
+        };
         break;
-      case 'furniture':
-        formData = { ...formData, height: data.height ? Number(data.height) : null, width: data.width ? Number(data.width) : null, length: data.length ? Number(data.length) : null };
+      case "furniture":
+        formData = {
+          ...formData,
+          height: data.height ? Number(data.height) : null,
+          width: data.width ? Number(data.width) : null,
+          length: data.length ? Number(data.length) : null,
+        };
         break;
       default:
         break;
@@ -48,13 +53,10 @@ const NewProductForm = () => {
     try {
       await fetchData(API_URL + "/products/new", formData);
       mutate(API_URL + "/products/exhibit");
-      // useStore.getState().fetchAndUpdateProducts();
-      navigate("/product_list"); 
+      navigate("/product_list");
     } catch (error) {
       console.error("Error:", error);
     }
-    
-
   };
 
   const formElements = [
@@ -63,11 +65,17 @@ const NewProductForm = () => {
       id: "sku",
       type: "text",
       title: "SKU",
-      rules: { required: true, minLength: 5, maxLength: 20 },
+      rules: {
+        required: true,
+        minLength: 5,
+        maxLength: 20,
+        pattern: /^[a-zA-Z0-9_-]+$/,
+      },
       errorMessage: {
         required: "Please, submit required data",
         minLength: "This field requires minimum 5 characters",
         maxLength: "This field can not exceed 20 characters",
+        pattern: "Special characters are not allowed except _ and -",
       },
     },
     {
@@ -75,11 +83,17 @@ const NewProductForm = () => {
       id: "name",
       type: "text",
       title: "Name",
-      rules: { required: true, minLength: 3, maxLength: 15 },
+      rules: {
+        required: true,
+        minLength: 3,
+        maxLength: 100,
+        pattern: /^[a-zA-Z0-9 _-]+$/,
+      },
       errorMessage: {
         required: "Please, submit required data",
         minLength: "This field requires minimum 3 characters",
-        maxLength: "This field can not exceed 15 characters",
+        maxLength: "This field can not exceed 100 characters",
+        pattern: "Special characters are not allowed except _ and -",
       },
     },
     {
@@ -87,11 +101,17 @@ const NewProductForm = () => {
       id: "price",
       type: "number",
       title: "Price ($)",
-      rules: { required: true, min: 0, max: 9999 },
+      rules: {
+        required: true,
+        min: 0,
+        max: 9999,
+        pattern: /^[0-9]+$/,
+      },
       errorMessage: {
         required: "Please, submit required data",
         min: "Can not go lower than 1",
         max: "Can not go higher than 9999",
+        pattern: "Only numeric values are allowed",
       },
     },
   ];
@@ -102,9 +122,8 @@ const NewProductForm = () => {
       onSubmit={handleSubmit(onSubmit)}
       id="product_form"
     >
-
       {isLoading ? (
-        <span className="loading loading-ball loading-md"></span>
+        <span className="loading loading-ball loading-lg"></span>
       ) : error ? (
         <div className="alert alert-error w-1/6">
           <svg
@@ -122,156 +141,159 @@ const NewProductForm = () => {
           </svg>
           <span>{error.message}</span>
         </div>
-      ) : formElements.map((item) => {
-        return (
-          <Input
-            key={item.key}
-            id={item.id}
-            type={item.type}
-            title={item.title}
-            register={register}
-            formState={formState}
-            rules={item.rules}
-            required
-            errorMessage={item.errorMessage}
-          >
-            <h3>{item.title}</h3>
-          </Input>
-        );
-      })}
-
-      <div className="relative w-5/6 lg:w-4/6 border-2 border-black flex justify-between items-center">
-        <label
-          htmlFor="productType"
-          className="pl-1"
-        >
-          Type:
-        </label>
-        <select
-          className="p-1 bg-white text-center w-4/5"
-          name="productType"
-          id="productType"
-          {...register("productType", { required: true, defaultValue: "" })}
-        >
-          <option
-            value=""
-            disabled
-            selected
-          >
-            Select Type
-          </option>
-          <option value="dvd">DVD</option>
-          <option value="furniture">Furniture</option>
-          <option value="book">Book</option>
-        </select>
-      </div>
-
-      {productType === "dvd" && (
-        <DVDProperties
-          id="size"
-          type="number"
-          register={register}
-          formState={formState}
-          rules={{ required: true, min: 0, max: 4500 }}
-          errorMessage={{
-            required: "Please, submit required data",
-            min: "Can not go lower than 1",
-            max: "Can not go higher than 4500mb",
-          }}
-          title="Size"
-          required
-        />
-      )}
-      {productType === "book" && (
-        <BookProperties
-          id="weight"
-          type="number"
-          register={register}
-          formState={formState}
-          rules={{ required: true, min: 0, max: 50 }}
-          errorMessage={{
-            required: "Please, submit required data",
-            min: "Can not go lower than 1",
-            max: "Can not go higher than 50kg",
-          }}
-          title="Weight"
-          required
-        />
-      )}
-      {productType === "furniture" && (
+      ) : (
         <>
-          <Input
-            key="1"
-            id="height"
-            type="number"
-            register={register}
-            formState={formState}
-            rules={{ required: true, min: 0, max: 20000 }}
-            required
-            errorMessage={{
-              required: "Please, submit required data",
-              min: "Can not go lower than 0",
-              max: "Can not go higher than 20000",
-            }}
-          >
-            <h3>Height</h3>
-          </Input>
-          <Input
-            key="2"
-            id="width"
-            type="number"
-            register={register}
-            formState={formState}
-            rules={{ required: true, min: 0, max: 20000 }}
-            required
-            errorMessage={{
-              required: "Please, submit required data",
-              min: "Can not go lower than 0",
-              max: "Can not go higher than 20000",
-            }}
-          >
-            <h3>Width</h3>
-          </Input>
-          <Input
-            key="3"
-            id="length"
-            type="number"
-            register={register}
-            formState={formState}
-            rules={{ required: true, min: 0, max: 20000 }}
-            required
-            errorMessage={{
-              required: "Please, submit required data",
-              min: "Can not go lower than 0",
-              max: "Can not go higher than 20000",
-            }}
-          >
-            <h3>Length</h3>
-          </Input>
-          <p>
-            Please provide dimensions in height, width and length (cm) format.
-          </p>
+          {formElements.map((item) => {
+            return (
+              <Input
+                key={item.key}
+                id={item.id}
+                type={item.type}
+                title={item.title}
+                register={register}
+                formState={formState}
+                rules={item.rules}
+                required
+                errorMessage={item.errorMessage}
+              >
+                <h3>{item.title}</h3>
+              </Input>
+            );
+          })}
+          <div className="relative w-5/6 lg:w-4/6 border-2 border-black flex justify-between items-center">
+            <label
+              htmlFor="productType"
+              className="pl-1"
+            >
+              Type:
+            </label>
+            <select
+              className="p-1 bg-white text-center w-4/5"
+              name="productType"
+              id="productType"
+              {...register("productType", { required: true })}
+            >
+              <option
+                value=""
+                disabled
+                selected
+              >
+                Select Type
+              </option>
+              <option value="dvd">DVD</option>
+              <option value="furniture">Furniture</option>
+              <option value="book">Book</option>
+            </select>
+          </div>
+
+          {productType === "dvd" && (
+            <DVDProperties
+              id="size"
+              type="number"
+              register={register}
+              formState={formState}
+              rules={{ required: true, min: 0, max: 4500 }}
+              errorMessage={{
+                required: "Please, submit required data",
+                min: "Can not go lower than 1",
+                max: "Can not go higher than 4500mb",
+              }}
+              title="Size"
+              required
+            />
+          )}
+          {productType === "book" && (
+            <BookProperties
+              id="weight"
+              type="number"
+              register={register}
+              formState={formState}
+              rules={{ required: true, min: 0, max: 50 }}
+              errorMessage={{
+                required: "Please, submit required data",
+                min: "Can not go lower than 1",
+                max: "Can not go higher than 50kg",
+              }}
+              title="Weight"
+              required
+            />
+          )}
+          {productType === "furniture" && (
+            <>
+              <Input
+                key="1"
+                id="height"
+                type="number"
+                register={register}
+                formState={formState}
+                rules={{ required: true, min: 0, max: 20000 }}
+                required
+                errorMessage={{
+                  required: "Please, submit required data",
+                  min: "Can not go lower than 0",
+                  max: "Can not go higher than 20000",
+                }}
+              >
+                <h3>Height</h3>
+              </Input>
+              <Input
+                key="2"
+                id="width"
+                type="number"
+                register={register}
+                formState={formState}
+                rules={{ required: true, min: 0, max: 20000 }}
+                required
+                errorMessage={{
+                  required: "Please, submit required data",
+                  min: "Can not go lower than 0",
+                  max: "Can not go higher than 20000",
+                }}
+              >
+                <h3>Width</h3>
+              </Input>
+              <Input
+                key="3"
+                id="length"
+                type="number"
+                register={register}
+                formState={formState}
+                rules={{ required: true, min: 0, max: 20000 }}
+                required
+                errorMessage={{
+                  required: "Please, submit required data",
+                  min: "Can not go lower than 0",
+                  max: "Can not go higher than 20000",
+                }}
+              >
+                <h3>Length</h3>
+              </Input>
+              <p>
+                Please provide dimensions in height, width and length (cm)
+                format.
+              </p>
+            </>
+          )}
+
+          <div className="flex gap-10">
+            <Button
+              type="button"
+              className="btn btn-secondary"
+              onClick={handleCancel}
+            >
+              Cancel
+            </Button>
+            <Button
+              type="submit"
+              className="btn btn-primary"
+            >
+              Save
+            </Button>
+          </div>
         </>
       )}
-
-      <div className="flex gap-10">
-        <Button
-          type="button"
-          className="btn btn-secondary"
-          onClick={handleCancel}
-        >
-          Cancel
-        </Button>
-        <Button
-          type="submit"
-          className="btn btn-primary"
-        >
-          Save
-        </Button>
-      </div>
     </form>
   );
-}
-
+};
 
 export default NewProductForm;
